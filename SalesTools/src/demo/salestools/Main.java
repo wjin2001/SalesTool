f5411a9f95df4059febc28a8e6f08dc3e98d6e42
Wei Jin <wjin_2001@yahoo.com>
2015-02-28 19:45:28 -0500

    Initial Checkin of Sales Tool.

---
+++ SalesTools/src/demo/salestools/Main.java
@@ -0,0 +1,16 @@
+package demo.salestools;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APLL SHOWS SALES DATA!");
+	}
+}
---
+++ SalesTools/src/demo/salestools/SalesData.java
@@ -0,0 +1,9 @@
+package demo.salestools;
+
+public class SalesData {
+	int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
